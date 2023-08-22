e77c1ff3e56e5a72ee32c2e7d295fdea0640fb12
Lukas Rotermund <lukas.rotermund@tasko.de>
2023-08-22 09:26:35 +0200

    added 2 funcs to toggle between PHP class and test

--- lua/lrotermund/snippets.lua
+++ lua/lrotermund/snippets.lua
@@ -41,5 +41,61 @@ function InteractiveInsertClassSkeleton()
     vim.api.nvim_feedkeys('i' .. class_code, 'n', true)
 end
 
+function GotoPHPTest()
+    -- /home/lukas/Repositories/foobar/src/Service/foo.php
+    -- --> /home/lukas/Repositories/foobar/tests/Service/fooTest.php
+    local path = vim.api.nvim_buf_get_name(0)
+
+    if not path:match("/src/") or not path:match("%.php$") then
+        print('no PHP class')
+
+        return false
+    end
+
+    local testPath = string.gsub(
+        string.gsub(path, '/src/', '/tests/'),
+        '.php',
+        'Test.php'
+    )
+    local f = io.open(testPath, 'r')
+
+    if f then
+        f:close()
+        vim.cmd("edit " .. testPath)
+    else
+        vim.cmd("enew")
+        vim.cmd("file " .. testPath)
+    end
+end
+
+function GotoPHPClass()
+    -- /home/lukas/Repositories/foobar/tests/Service/fooTest.php
+    -- --> /home/lukas/Repositories/foobar/src/Service/foo.php
+    local path = vim.api.nvim_buf_get_name(0)
+
+    if not path:match("/tests/") or not path:match("%Test.php$") then
+        print('no PHP test')
+
+        return false
+    end
+
+    local classPath = string.gsub(
+        string.gsub(path, '/tests/', '/src/'),
+        'Test.php',
+        '.php'
+    )
+    local f = io.open(classPath, 'r')
+
+    if f then
+        f:close()
+        vim.cmd("edit " .. classPath)
+    else
+        vim.cmd("enew")
+        vim.cmd("file " .. classPath)
+    end
+end
+
 vim.cmd([[command! -nargs=0 Ifs lua InteractiveInsertFunctionSkeleton()]])
 vim.cmd([[command! -nargs=0 Ics lua InteractiveInsertClassSkeleton()]])
+vim.cmd([[command! -nargs=0 Ptest lua GotoPHPTest()]])
+vim.cmd([[command! -nargs=0 Pclass lua GotoPHPClass()]])
