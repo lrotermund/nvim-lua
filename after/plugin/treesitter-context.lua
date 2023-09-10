21d8275b61469133acc5666384ec802943a1820b
Lukas Rotermund <lukas.rotermund@tasko.de>
2023-09-10 18:53:22 +0200

    reduced treesitter context lines and impr php cmds

--- after/plugin/treesitter-context.lua
+++ after/plugin/treesitter-context.lua
@@ -1,3 +1,7 @@
+require'treesitter-context'.setup{
+  max_lines = 4, -- How many lines the window should span. Values <= 0 mean no limit.
+}
+
 vim.keymap.set("n", "[c", function()
   require("treesitter-context").go_to_context()
 end, { silent = true })
--- lua/lrotermund/snippets.lua
+++ lua/lrotermund/snippets.lua
@@ -1,27 +1,42 @@
+function Is_php_or_go_file()
+    local path = vim.api.nvim_buf_get_name(0)
+    if not path:match("%.go") or not path:match("%.php$") then
+        print('no Go or PHP file')
 
-function InteractiveInsertFunctionSkeleton()
-    local language = vim.fn.input('Choose a programming language (php/go): ')
+        return false
+    end
+
+    return true
+end
+
+function Interactive_insert_function_skeleton()
+    if not Is_php_or_go_file then
+        return false
+    end
+
+    local path = vim.api.nvim_buf_get_name(0)
     local function_name = vim.fn.input('Choose a name for the function: ')
     local function_code = ''
 
-    if language == 'php' then
+    if path:match("%.php") then
         function_code = string.format("private function %s(): void\n{\n\n}", function_name)
-    elseif language == 'go' then
-        function_code = string.format("func %s() {\n\t\n}", function_name)
     else
-        print('Unsupported programming language')
-        return
+        function_code = string.format("func %s() {\n\t\n}", function_name)
     end
 
     vim.api.nvim_feedkeys('i' .. function_code, 'n', true)
 end
 
-function InteractiveInsertClassSkeleton()
-    local language = vim.fn.input('Choose a programming language (php/go): ')
+function Interactive_insert_class_skeleton()
+    if not Is_php_or_go_file then
+        return false
+    end
+
+    local path = vim.api.nvim_buf_get_name(0)
     local class_name = ''
     local class_code = ''
 
-    if language == 'php' then
+    if path:match("%.php") then
         class_name = vim.fn.input('Choose a name for the class: ')
         local namesspace_name = vim.fn.input('Choose a namespace: ')
 
@@ -30,18 +45,15 @@ function InteractiveInsertClassSkeleton()
             namesspace_name,
             class_name
         )
-    elseif language == 'go' then
+    else
         class_name = vim.fn.input('Choose a name for the struct: ')
         class_code = string.format("type %s struct {\n\t\n}", class_name)
-    else
-        print('Unsupported programming language')
-        return
     end
 
     vim.api.nvim_feedkeys('i' .. class_code, 'n', true)
 end
 
-function GotoPHPTest()
+function Goto_php_test()
     -- /home/lukas/Repositories/foobar/src/Service/foo.php
     -- --> /home/lukas/Repositories/foobar/tests/Service/fooTest.php
     local path = vim.api.nvim_buf_get_name(0)
@@ -68,8 +80,36 @@ function GotoPHPTest()
     end
 end
 
-function GotoPHPClass()
+function Goto_php_mockery_test()
+    -- /home/lukas/Repositories/foobar/src/Service/foo.php
+    -- --> /home/lukas/Repositories/foobar/tests/Service/fooMockeryTest.php
+    local path = vim.api.nvim_buf_get_name(0)
+
+    if not path:match("/src/") or not path:match("%.php$") then
+        print('no PHP class')
+
+        return false
+    end
+
+    local testPath = string.gsub(
+        string.gsub(path, '/src/', '/tests/'),
+        '.php',
+        'MockeryTest.php'
+    )
+    local f = io.open(testPath, 'r')
+
+    if f then
+        f:close()
+        vim.cmd("edit " .. testPath)
+    else
+        vim.cmd("enew")
+        vim.cmd("file " .. testPath)
+    end
+end
+
+function Goto_php_class()
     -- /home/lukas/Repositories/foobar/tests/Service/fooTest.php
+    -- or /home/lukas/Repositories/foobar/tests/Service/fooMockeryTest.php
     -- --> /home/lukas/Repositories/foobar/src/Service/foo.php
     local path = vim.api.nvim_buf_get_name(0)
 
@@ -90,12 +130,25 @@ function GotoPHPClass()
         f:close()
         vim.cmd("edit " .. classPath)
     else
-        vim.cmd("enew")
-        vim.cmd("file " .. classPath)
+        local mockeryClassPath = string.gsub(
+            string.gsub(path, '/tests/', '/src/'),
+            'MockeryTest.php',
+            '.php'
+        )
+        local mf = io.open(mockeryClassPath, 'r')
+
+        if mf then
+            mf:close()
+            vim.cmd("edit " .. mockeryClassPath)
+        else
+            vim.cmd("enew")
+            vim.cmd("file " .. classPath)
+        end
     end
 end
 
-vim.cmd([[command! -nargs=0 Ifs lua InteractiveInsertFunctionSkeleton()]])
-vim.cmd([[command! -nargs=0 Ics lua InteractiveInsertClassSkeleton()]])
-vim.cmd([[command! -nargs=0 Ptest lua GotoPHPTest()]])
-vim.cmd([[command! -nargs=0 Pclass lua GotoPHPClass()]])
+vim.cmd([[command! -nargs=0 Ifs lua Interactive_insert_function_skeleton()]])
+vim.cmd([[command! -nargs=0 Ics lua Interactive_insert_class_skeleton()]])
+vim.cmd([[command! -nargs=0 Ptest lua Goto_php_test()]])
+vim.cmd([[command! -nargs=0 Pmtest lua Goto_php_mockery_test()]])
+vim.cmd([[command! -nargs=0 Pclass lua Goto_php_class()]])
